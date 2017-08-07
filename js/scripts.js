e755cf67ed2612dcf56dfc4b1d421196c7f0e0bb
Austin Cummings and Margaret Berry <margaret@>
2017-08-07 10:31:52 -0700

    incorporate addition, subtraction, multiplication, division, convert celsius to fahrenheit Vice-versa. and link to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*var saySomething= function(whatToSay){
+var saySomething= function(whatToSay){
 alert(whatToSay);
 };
 
@@ -14,11 +14,30 @@ return number1/number2;
 var remainder = function(number1,number2){
 return number1% number2;
 };
-*/
 
 var add=function(number1,number2){
 return number1+number2;
 };
+
+//var number1 = parseInt(prompt("Enter a number:"));
+//var number2 = parseInt(prompt("Enter another number:"));
+
+
+var subtract=function(number1,number2){
+return number1-number2;
+};
+
+var celsiusToFahrenheit=function(number1){
+  return number1*1.8+32;
+};
+
+var fahrenheitToCelsius=function(number1){
+  return (number1-32)/1.8;
+};
+
+
+// user prompts below
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1,number2));
+var result = fahrenheitToCelsius(number1);
+alert(result);
