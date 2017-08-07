f12ce8e5b7940debd0599303d1445833657f4547
Austin Cummings and Margaret Berry <margaret@>
2017-08-07 10:04:28 -0700

    initialize an html, jss, css directory along with the begininings of a calculator

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+/*var saySomething= function(whatToSay){
+alert(whatToSay);
+};
+
+var multiply=function(number1,number2){
+return number1*number2;
+};
+
+var divide=function(number1,number2){
+return number1/number2;
+};
+
+
+var remainder = function(number1,number2){
+return number1% number2;
+};
+*/
+
+var add=function(number1,number2){
+return number1+number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1,number2));
