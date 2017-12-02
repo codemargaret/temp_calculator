812423e0d09bae2b007ba9e024bc61eeeb90d21c
Margaret Berry <codeberry1@gmail.com>
2017-12-01 19:50:17 -0800

    remove extra number prompt so app can run

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,36 @@
-var saySomething= function(whatToSay){
-alert(whatToSay);
+var saySomething = function(whatToSay){
+  alert(whatToSay);
 };
 
-var multiply=function(number1,number2){
-return number1*number2;
+var multiply = function(number1,number2){
+  return number1*number2;
 };
 
-var divide=function(number1,number2){
-return number1/number2;
+var divide = function(number1,number2){
+  return number1/number2;
 };
-
 
 var remainder = function(number1,number2){
-return number1% number2;
+  return number1% number2;
 };
 
-var add=function(number1,number2){
-return number1+number2;
+var add = function(number1,number2){
+  return number1+number2;
 };
 
-//var number1 = parseInt(prompt("Enter a number:"));
-//var number2 = parseInt(prompt("Enter another number:"));
-
-
-var subtract=function(number1,number2){
-return number1-number2;
+var subtract = function(number1,number2){
+  return number1-number2;
 };
 
-var celsiusToFahrenheit=function(number1){
+var celsiusToFahrenheit = function(number1){
   return number1*1.8+32;
 };
 
-var fahrenheitToCelsius=function(number1){
+var fahrenheitToCelsius = function(number1){
   return (number1-32)/1.8;
 };
 
-
 // user prompts below
 var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
 var result = fahrenheitToCelsius(number1);
 alert(result);
